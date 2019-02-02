c2c60b6bef5b622472ae735fcaaa8068acbd2692
alexaladov-codefresh <44968728+alexaladov-codefresh@users.noreply.github.com>
2019-02-02 18:52:29 +0300

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM alpine:3.8
-
-RUN apk add \
-    bash jq py-pip \
-&&  pip install yq
-
-COPY ./script.sh /script.sh
+RUN apk add bash git curl jq py-pip && pip install yq
+COPY script.sh /script.sh
 RUN chmod +x /script.sh
-
-CMD ["/script.sh"] 
+CMD ["/script.sh"]
--- script.sh
+++ script.sh
@@ -9,30 +9,53 @@ NC='\033[0m' # No Color
 part(){
        truncate -s 0 /tmp/result.json
        cat "$1$2" | yq --argjson pipeline "$(cat $1$3 | yq .)" \
-       '{"id":"test","title":"test","source":.sources[1],"description":.description,"category":"test","handle":"test","create":"test","author":"test","view_count":"0","usage_sample":[{"content":$pipeline},{"variable":.envs}]}'> /tmp/result.json
+       --arg uuid "$( cat "$1$2" | yq -r  '.title' | tr '[:upper:]' '[:lower:]' | sed -e 's/ /-/g')" \
+       --arg created "$(date -d @"$(git log --format=%at $1$2 | tail -1)" +%FT%T.000Z)" \
+       '{"id":$uuid,"title":.title,"source":.source,"description":.description,"category":.category,"handle":$uuid,"create":$created,"author":.maintainer[0].name,"view_count":"0","usage_sample":[{"content":$pipeline},{"variable":.envs}]}'> /tmp/result.json
        cat '/tmp/step.json' | jq -r --argjson pipeline "$(cat '/tmp/result.json' | jq .)" '. +=  [$pipeline]' > /tmp/step.json
       }
 
+logo_download(){
+                LOGO_FILENAME="$(cat "$1$2" | yq -r  '.title' | tr '[:upper:]' '[:lower:]' | sed -e 's/ /-/g')"
+                LOGO_LINK="$(cat "$1$2" | yq -r  '.logo' )"
+                STATUS="$(curl -s -o /dev/null -I -w "%{http_code}" ${LOGO_LINK})"
+                if [ "$STATUS" == "200" ]; then
+                  curl -s  "${LOGO_LINK}" --output "${BASE_DIR}/logos/${LOGO_FILENAME}.jpg"  # any image type needs to have a .jpg extension by the app design
+                  echo -e "Logo for the plugin ${LOGO_FILENAME} ${GREEN}Downloaded${NC}"
+                else
+                   echo -e "Error accessing logo for the plugin ${LOGO_FILENAME} Error: ${RED}${STATUS}${NC}"
+                   exit 1
+                fi
+                }
+
 yaml_check(){
              if [ -f "$1$2" ]; then
-                   yq . "$1$2" >/dev/null 2>/dev/null \
-                   && echo -e "$1$2 <--YAML Syntax check ${GREEN}OK${NC}" \
-                   || echo -e "$1$2 <--YAML Syntax check ${RED}FAILED${NC}" \
-                   && flag=0
+                   if yq . "$1$2" >/dev/null 2>/dev/null; then
+                      echo -e "$1$2 <--YAML Syntax check ${GREEN}OK${NC}"
+                   else echo -e "$1$2 <--YAML Syntax check ${RED}FAILED${NC}"
+                        result='fail'
+                   fi
              else
                    echo -e "${RED}Please create file${NC} $1plugin.yaml"
-                   flag=0
+                   result='fail'
              fi
              if [ -f "$1$3" ]; then
-                   yq . "$1$3" >/dev/null 2>/dev/null \
-                   && echo -e "$1$3 <--YAML Syntax check ${GREEN}OK${NC}" \
-                   || echo -e "$1$3 <--YAML Syntax check ${RED}FAILED${NC}" \
-                   && flag=0
+                   if yq . "$1$3" >/dev/null 2>/dev/null; then
+                      echo -e "$1$3 <--YAML Syntax check ${GREEN}OK${NC}"
+                   else echo -e "$1$3 <--YAML Syntax check ${RED}FAILED${NC}"
+                        result='fail'
+                   fi
              else
-                   echo -e "${RED}Please create file${NC} $1pipeline.yaml"
-                   flag=0
+                   echo -e "${RED}Please create file${NC} $1example.yaml"
+                   result='fail'
              fi
              }
+
+replace_keys_with_ids(){
+                        cat "$1" | jq '.' | \
+                        jq --argjson arg "$( cat $2 | jq '[ .[] | {(.title) : (.id)} ] | add' )" \
+                        '.[] | .category as $key | if $arg | has($key[0]) then  .category|=[$arg[$key[]]] else .category|=[$arg["Featured"]] end'| jq -s '.' > $1
+                       }
 
 
 ###########################################################
@@ -49,12 +72,13 @@ echo -e "Syntax check started..."
 echo -e "-----------------------${NC}"
 
 for d in ${BASE_DIR}/plugins/*/; do
-pipeline=$(basename $d/pipeline.y*)
+pipeline=$(basename $d/example.y*)
 plugin=$(basename $d/plugin.y*)
   yaml_check $d $plugin $pipeline
 done
 
-if [ flag==0 ]; then
+
+if [ "$result" == "fail" ]; then
 echo -e "${RED}-----------------------------------------"
 echo -e "Please correct found problems and restart"
 echo -e "-----------------------------------------${NC}"
@@ -62,18 +86,26 @@ exit 1
 fi
 
 
-#making step.json file
+#making step.json file and downloading logos
+mkdir -p ${BASE_DIR}/logos/
 for d in ${BASE_DIR}/plugins/*/; do
     echo "Processing Dir -> $d"
-  pipeline=$(basename $d/pipeline.y*)
+  pipeline=$(basename $d/example.y*)
   #echo "$pipeline"
   plugin=$(basename $d/plugin.y*)
   #echo "$plugin"
     if [ -f "$d/$pipeline" ] && [ -f "$d/$plugin" ]; then
       part $d $plugin $pipeline
+      logo_download $d $plugin
     fi
   pipeline=''
   plugin=''
 done
 
-cp /tmp/step.json ${BASE_DIR}/step.json
+#replace keys with ids
+#cat /tmp/step.json
+replace_keys_with_ids /tmp/step.json $BASE_DIR/category.json
+
+#copy step.json to a base dir
+cp /tmp/step.json $BASE_DIR/step.json
+#cat $BASE_DIR/step.json
